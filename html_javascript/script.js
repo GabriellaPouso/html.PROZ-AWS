bd2f98f7b8c72dec5444febe376e192725405b04
Gabriella Pouso <pouso.gabi@gmail.com>
2024-11-20 13:16:18 -0300

    HTML+JAVASCRIPT

---
+++ html_javascript/script.js
@@ -0,0 +1,9 @@
+// Define a função que imprime a mensagem no terminal
+function mostrarMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executa a função três vezes
+mostrarMensagem();
+mostrarMensagem();
+mostrarMensagem();
